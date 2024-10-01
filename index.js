1913101dd41a0d0418bf9d6ac887d6519011863f
Dawar-codes <dawar.malik29@gmail.com>
2024-10-01 12:09:54 +0500

    add connection string

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME, // Use the name of your database here
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432, // Use 5432 as a default if not set
+  connectionString: process.env.DATABASE_URL,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME, // Use the name of your database here
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT || 5432, // Use 5432 as a default if not set
 });
   
   db.connect();
