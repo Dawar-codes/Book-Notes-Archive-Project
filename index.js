27be42e854348a87ba99f43a66b6e59eb2d88ece
Dawar-codes <dawar.malik29@gmail.com>
2024-10-01 14:00:00 +0500

    port

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 3000;
 const db = new pg.Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  port: 5432,
   // user: process.env.DB_USER,
   // host: process.env.DB_HOST,
   // database: process.env.DB_NAME, // Use the name of your database here
